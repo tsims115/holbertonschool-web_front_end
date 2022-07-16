c75a769e30262c7f73e183f1fccec06569f2875e
Trenton Sims <tsims115@yahoo.com>
2022-07-16 17:31:09 -0500

    Adding tasks 8

--- 0x00-Javascript_advanced/8-payments.js
+++ 0x00-Javascript_advanced/8-payments.js
@@ -5,7 +5,7 @@ function processPayment(amount) {
 function processOrder(orderId, amount) {
     console.log(`${orderId} is being processed`);
     processPayment(amount);
-    console.log("${orderId} has been fully processed");
+    console.log(`${orderId} has been fully processed`);
 }
 
 console.log("Processing orders");
